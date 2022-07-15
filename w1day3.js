b8a1d861b06a595ad8febd54f21052e0777f2457
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-14 21:24:20 -0400

    commented homework

---
+++ w1day3.js
@@ -0,0 +1,10 @@
+// 1. Write a function named assignGrade that takes one argument, a number score.
+// Returns a grade for the score, either “A,” “B,” “C,” “D,” or “F.” Call that function for a few different scores and log the result to ensure it works.
+// 2. Create a function that returns the number of true values in an array.
+// const testArray = [ true, false, false, true, false, false, false, true, true, true, false ]
+// 3. Create a function that converts a string to all uppercase letters and console.log the result (hint: string methods):
+// let str = “WHAT a TIME to BE ALIVE!”
+// 4. Create a function that takes in a number of two-pointers and three-pointers made and returns a basketball team’s total score?
+// Extra for experts:
+// 5. Create a function that takes an array of numbers & returns both min and max in that order. (use a loop)
+
