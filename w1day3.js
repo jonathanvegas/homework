60383e738f7b9df39ee083ecc87178030c3696d3
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-14 22:05:04 -0400

    homework

--- w1day3.js
+++ w1day3.js
@@ -1,22 +1,21 @@
-// 1. Write a function named assignGrade that takes one argument, a number score.
-// Returns a grade for the score, either “A,” “B,” “C,” “D,” or “F.” Call that function for a 
-// few different scores and log the result to ensure it works.
-// 2. Create a function that returns the number of true values in an array.
-// const testArray = [ true, false, false, true, false, false, false, true, true, true, false ]
-// 3. Create a function that converts a string to all uppercase letters and console.log the result 
-// (hint: string methods):
-// let str = “WHAT a TIME to BE ALIVE!”
-// 4. Create a function that takes in a number of two-pointers and three-pointers made and returns a 
-// basketball team’s total score?
+
+
+
+
 // Extra for experts:
 // 5. Create a function that takes an array of numbers & returns both min and max in that order. (use a loop)
 
-// 1.
+// 1. Write a function named assignGrade that takes one argument, a number score.
+// Returns a grade for the score, either “A,” “B,” “C,” “D,” or “F.” Call that function for a 
+// few different scores and log the result to ensure it works.
+
 // function assignGrade(score){
 
 // }
 
-//2.
+// 2. Create a function that returns the number of true values in an array.
+// const testArray = [ true, false, false, true, false, false, false, true, true, true, false ]
+
 const testArray = [ true, false, false, true, false, false, false, true, true, true, false ]
 let sum = 0;
 function trueValues(testArray){
@@ -29,9 +28,20 @@ function trueValues(testArray){
 }
 console.log("True Values: " + trueValues(testArray))
 
-//3.
+// 3. Create a function that converts a string to all uppercase letters and console.log the result 
+// (hint: string methods): let str = “WHAT a TIME to BE ALIVE!”
+
 let str = "WHAT a TIME to BE ALIVE!"
 function allUppercase(str){
     return(str.toUpperCase())
 }
 console.log(allUppercase(str))
+
+// 4. Create a function that takes in a number of two-points and three-points made and returns a 
+// basketball team’s total score?
+let num2Points = 25
+let num3Points = 12
+function totalScore(num2Points, num3Points){
+    return (num2Points * 2) + (num3Points * 3)
+}
+console.log("Total Score: " + totalScore(num2Points,num3Points))
